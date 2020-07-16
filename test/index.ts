1fa84f4b57b05720d061e4c0504119fbfb3138d9
Wayne Van Son <waynevanson@gmail.com>
2020-07-16 15:18:36 +1000

    tests: implement tests for withLogger

--- test/index.ts
+++ test/index.ts
@@ -1,6 +1,13 @@
 import * as assert from 'assert'
 import * as C from 'fp-ts/lib/Console'
 import * as L from '../src/IO'
+import { withLogger } from '../src'
+import * as IO from 'fp-ts/lib/IO'
+import { pipe } from 'fp-ts/lib/pipeable'
+import * as T from 'fp-ts/lib/Task'
+import * as IOE from 'fp-ts/lib/IOEither'
+import * as TE from 'fp-ts/lib/TaskEither'
+import * as E from 'fp-ts/lib/Either'
 
 describe('Logger', () => {
   let ledger: Array<number>
@@ -41,5 +48,78 @@ describe('Logger', () => {
     logger('a')()
     logger('aaa')()
     assert.deepEqual(ledger, ['aaa'])
+  })
+})
+
+describe('withLogger', () => {
+  let ledger: Array<number>
+  let consolelog: any
+
+  beforeEach(() => {
+    ledger = []
+    consolelog = console.log
+    console.log = (message: any) => ledger.push(message)
+  })
+
+  afterEach(() => {
+    console.log = consolelog
+  })
+
+  describe('sync', () => {
+    test('MonadIO1', () => {
+      const ioLog = withLogger(IO.io)(C.log)
+
+      const result = pipe(
+        IO.of(2),
+        ioLog((a) => a + 5)
+      )()
+
+      assert.equal(result, 2)
+      assert.deepEqual(ledger, [7])
+    })
+
+    test('MonadIO2', () => {
+      const ioEitherLog = withLogger(IOE.ioEither)(C.log)
+
+      const result = pipe(
+        IOE.right(2),
+        ioEitherLog((a) => a + 5)
+      )()
+
+      assert.deepStrictEqual(result, E.right(2))
+      assert.deepEqual(ledger, [7])
+    })
+
+    test.skip('MonadIO2C', () => {})
+    test.skip('MonadIO3', () => {})
+  })
+
+  describe('asynchronous', () => {
+    test('MonadIO1', async () => {
+      const tlog = withLogger(T.task)(C.log)
+
+      const result = await pipe(
+        T.of(2),
+        tlog((a) => a + 5)
+      )()
+
+      assert.equal(result, 2)
+      assert.deepEqual(ledger, [7])
+    })
+
+    test('MonadIO2', async () => {
+      const ioEitherLog = withLogger(TE.taskEither)(C.log)
+
+      const result = await pipe(
+        TE.right(2),
+        ioEitherLog((a) => a + 5)
+      )()
+
+      assert.deepStrictEqual(result, E.right(2))
+      assert.deepEqual(ledger, [7])
+    })
+
+    test.skip('MonadIO2C', () => {})
+    test.skip('MonadIO3', () => {})
   })
 })
