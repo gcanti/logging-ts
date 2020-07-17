dd62b9e291201d743a08d171e5ea2b45cf13df79
Wayne Van Son <waynevanson@gmail.com>
2020-07-17 13:34:30 +1000

    tests: remove uneccessary tests

--- test/index.ts
+++ test/index.ts
@@ -4,10 +4,6 @@ import * as L from '../src/IO'
 import { withLogger } from '../src/IO'
 import * as IO from 'fp-ts/lib/IO'
 import { pipe } from 'fp-ts/lib/pipeable'
-import * as T from 'fp-ts/lib/Task'
-import * as IOE from 'fp-ts/lib/IOEither'
-import * as TE from 'fp-ts/lib/TaskEither'
-import * as E from 'fp-ts/lib/Either'
 
 describe('Logger', () => {
   let ledger: Array<number>
@@ -77,49 +73,5 @@ describe('withLogger', () => {
       assert.equal(result, 2)
       assert.deepEqual(ledger, [7])
     })
-
-    test('MonadIO2', () => {
-      const ioEitherLog = withLogger(IOE.ioEither)(C.log)
-
-      const result = pipe(
-        IOE.right(2),
-        ioEitherLog((a) => a + 5)
-      )()
-
-      assert.deepStrictEqual(result, E.right(2))
-      assert.deepEqual(ledger, [7])
-    })
-
-    test.skip('MonadIO2C', () => {})
-    test.skip('MonadIO3', () => {})
-  })
-
-  describe('asynchronous', () => {
-    test('MonadIO1', async () => {
-      const tlog = withLogger(T.task)(C.log)
-
-      const result = await pipe(
-        T.of(2),
-        tlog((a) => a + 5)
-      )()
-
-      assert.equal(result, 2)
-      assert.deepEqual(ledger, [7])
-    })
-
-    test('MonadIO2', async () => {
-      const ioEitherLog = withLogger(TE.taskEither)(C.log)
-
-      const result = await pipe(
-        TE.right(2),
-        ioEitherLog((a) => a + 5)
-      )()
-
-      assert.deepStrictEqual(result, E.right(2))
-      assert.deepEqual(ledger, [7])
-    })
-
-    test.skip('MonadIO2C', () => {})
-    test.skip('MonadIO3', () => {})
   })
 })
